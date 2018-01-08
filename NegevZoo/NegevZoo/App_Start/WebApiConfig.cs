bf7cc94bb065c0d965458f51f5afd390e46237c7
orp <oripapo@gmail.com>
2018-01-08 21:00:29 +0200

    Initialized DummyDB for development reasons. Added enclosures and animal fakes. Constructed animal controller.

--- NegevZoo/NegevZoo/App_Start/WebApiConfig.cs
+++ NegevZoo/NegevZoo/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace NegevZoo
@@ -16,6 +17,8 @@ namespace NegevZoo
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
