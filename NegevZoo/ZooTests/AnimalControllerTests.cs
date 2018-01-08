615e110d06154ffb09c6a22c22ae7e02a642653e
orp <oripapo@gmail.com>
2018-01-08 15:48:04 +0200

    Added server side project.

---
+++ NegevZoo/ZooTests/AnimalControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZooTests
+{
+    [TestClass]
+    public class AnimalControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
