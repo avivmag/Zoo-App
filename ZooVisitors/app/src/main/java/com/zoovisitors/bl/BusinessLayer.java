318c575e3a0cc234897dc7cbd2c8cb354024d3ad
gilorisr <gilorisr@post.bgu.ac.il>
2018-01-15 11:44:32 +0200

    Added news feed

--- ZooVisitors/app/src/main/java/com/zoovisitors/bl/BusinessLayer.java
+++ ZooVisitors/app/src/main/java/com/zoovisitors/bl/BusinessLayer.java
@@ -1,11 +1,8 @@
 package com.zoovisitors.bl;
 
 import com.zoovisitors.backend.AboutZoo;
-import com.zoovisitors.backend.Animal;
 import com.zoovisitors.backend.Enclosure;
-import com.zoovisitors.backend.NewsFeed;
 import com.zoovisitors.backend.Species;
-import com.zoovisitors.cl.network.ResponseInterface;
 
 /**
  * Created by Gili on 13/01/2018.
@@ -15,8 +12,8 @@ public interface BusinessLayer {
 
 
     public void getAnimals(int pos, final GetObjectInterface goi);
-    public Enclosure[] getEnclosures();
-    public AboutZoo getAboutZoo();
-    public NewsFeed getNewsFeed();
-    public Species getSpecies();
+    public void getEnclosures();
+    public void getAboutZoo();
+    public void getNewsFeed(final GetObjectInterface goi);
+    public void getSpecies();
 }
