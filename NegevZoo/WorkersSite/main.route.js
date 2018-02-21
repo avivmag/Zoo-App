2d535ec0d3f6e7e7a2ba15b7ae106130efd3ad41
orp <oripapo@gmail.com>
2018-02-21 19:07:14 +0200

    Initial web server commit.

---
+++ NegevZoo/WorkersSite/main.route.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module('visitors_app', ['ngMaterial', 'ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('login', {
+            url:            '/login',
+            templateUrl:    'login/login.html',
+            controller:     'loginCtrl'
+        })
+        .state('home', {
+            url:            '/mainMenu',
+            templateUrl:    'mainMenu/mainMenu.html',
+            controller:     'mainMenuCtrl'
+        })
+        .state('about', {
+            url:            '/about',
+            templateUrl:    'about.html'
+        })
+        .state('contact', {
+            url:            '/contact',
+            templateUrl:    'contact.html'
+        });
+});
