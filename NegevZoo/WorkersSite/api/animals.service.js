e8c0d2d21173aa022497d4f72303abda96f8eb42
orp <oripapo@gmail.com>
2018-03-12 11:41:46 +0200

    Fixed animals service bugs.

--- NegevZoo/WorkersSite/api/animals.service.js
+++ NegevZoo/WorkersSite/api/animals.service.js
@@ -6,7 +6,7 @@
         getAnimalById:          function (animalId, language)   { return httpService.httpGet({ url: [servicebaseUrl, 'id', animalId, language] }); },
         getAnimalsByEnclosure:  function (encId, language)      { return httpService.httpGet({ url: [servicebaseUrl, 'enclosure', encId, language] }); },
         getAnimalByName:        function (name, language)       { return httpService.httpGet({ url: [servicebaseUrl, 'name', name, language] }); },
-        updateAnimal:           function (enclosure)            { return httpService.httpPost({ url: [servicebaseUrl, 'update'], body: animal }); },
+        updateAnimal:           function (animal)               { return httpService.httpPost({ url: [servicebaseUrl, 'update'], body: animal }); },
         deleteAnimal:           function (animalId)             { return httpService.httpDelete({ url: [servicebaseUrl, 'delete', animalId] }); }
     };
 
