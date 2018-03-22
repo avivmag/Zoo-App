66a78d264cde19140bc23cb3a46ed3125f2797ff
gilabadi <gilabad@post.bgu.ac.il>
2018-03-22 22:09:06 +0200

    ops

---
+++ NegevZoo/DAL/Models/OpeningHourResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class OpeningHourResult
+    {
+        public int Id { get; set; }
+        public string Day { get; set; }
+        public long Language { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}
