0bcf6da590a9e5f6cbb16c2b049b3c821a6d30c5
orp <oripapo@gmail.com>
2018-05-09 17:21:37 +0300

    Added support for misc markers. Added option to upload a new map image. Exported map dialog to new controller.

--- NegevZoo/WorkersSite/api/map.service.js
+++ NegevZoo/WorkersSite/api/map.service.js
@@ -2,8 +2,11 @@
     var serviceBaseUrl  = 'map';
 
     var mapService      = {
-        getMap:         function ()             { return httpService.httpGet({ url: [serviceBaseUrl, 'url'] }); },
-        getAllMarkers:  function ()             { return httpService.httpGet({ url: [serviceBaseUrl, 'markers'] }); }
+        getMap:             function ()         { return httpService.httpGet({ url: [serviceBaseUrl, 'url'] }); },
+        getAllMarkers:      function ()         { return httpService.httpGet({ url: [serviceBaseUrl, 'markers'] }); },
+        getMiscMarkers:     function ()         { return httpService.httpGet({ url: [serviceBaseUrl, 'markers', 'misc'] }); },
+        updateMiscMarker:   function (marker)   { return httpService.httpPost({ url: [serviceBaseUrl, 'markers', 'misc', 'update'], body: marker }); },
+        deleteMiscMarker:   function (markerId) { return httpService.httpDelete({ url: [serviceBaseUrl, 'markers', markerId, 'delete'] }); }     
     };
 
     return mapService;
