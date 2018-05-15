996f5bd939a9623ce650a4a5ab205903676862f7
gilorisr <gilorisr@post.bgu.ac.il>
2018-05-16 00:03:42 +0300

    pop up added

---
+++ ZooVisitors/app/src/main/java/com/zoovisitors/pl/personalStories/PersonalPopUp.java
@@ -0,0 +1,51 @@
+package com.zoovisitors.pl.personalStories;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.zoovisitors.GlobalVariables;
+import com.zoovisitors.R;
+import com.zoovisitors.backend.Animal;
+import com.zoovisitors.pl.BaseActivity;
+
+public class PersonalPopUp extends BaseActivity {
+
+    private int width;
+    private int height;
+    private Animal.PersonalStories animal;
+    private Bundle clickedAnimal;
+    private TextView animalName;
+    private TextView animalStory;
+    private ImageView animalPic;
+    private Bitmap image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_pop_up);
+        image = (Bitmap) getIntent().getParcelableExtra("image");
+        clickedAnimal = getIntent().getExtras();
+        animal = (Animal.PersonalStories) clickedAnimal.getSerializable("animal");
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        width = dm.widthPixels;
+        height = dm.heightPixels;
+        getWindow().setLayout((int) (width * 0.8), (int) (height * 0.8));
+
+        animalName = (TextView) findViewById(R.id.personal_animal_name);
+        animalStory = (TextView) findViewById(R.id.personal_story);
+        animalPic = (ImageView) findViewById(R.id.personal_animal_pic);
+
+        animalName.setText(animal.getName());
+        animalStory.setText(animal.getStory());
+
+        if (image != null)
+            animalPic.setImageBitmap(image);
+        else
+            animalPic.setImageResource(R.mipmap.no_image_available);
+
+    }
+}
--- ZooVisitors/app/src/main/java/com/zoovisitors/pl/personalStories/PersonalStoriesRecyclerAdapter.java
+++ ZooVisitors/app/src/main/java/com/zoovisitors/pl/personalStories/PersonalStoriesRecyclerAdapter.java
@@ -1,17 +1,20 @@
 package com.zoovisitors.pl.personalStories;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.zoovisitors.GlobalVariables;
 import com.zoovisitors.R;
 import com.zoovisitors.backend.Animal;
+import com.zoovisitors.bl.callbacks.GetObjectInterface;
 import com.zoovisitors.pl.animals.AnimalActivity;
 import com.zoovisitors.pl.enclosures.EnclosureListRecyclerAdapter;
 
@@ -23,47 +26,34 @@ import java.util.List;
  */
 
 public class PersonalStoriesRecyclerAdapter extends RecyclerView.Adapter<PersonalStoriesRecyclerAdapter.ViewHolder> {
-    private String[] animalsImages;
-    private int[] images;
-    private Animal[] animals;
+    private Animal.PersonalStories[] personalStories;
+    private Bitmap[] images;
 
-    public PersonalStoriesRecyclerAdapter(String[] animalsImages, Animal[] animals) {
-        this.animalsImages = animalsImages;
-        this.animals = animals;
+    public PersonalStoriesRecyclerAdapter(Animal.PersonalStories[] personalStories) {
+        this.personalStories = personalStories;
+        images = new Bitmap[personalStories.length];
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
         public ImageView animal_card_image;
         public TextView animalName;
 
+
         public ViewHolder(View itemView){
             super(itemView);
-            List<Integer> imagesList = new ArrayList<Integer>();
-            for (String image : animalsImages) {
-                imagesList.add(GlobalVariables.appCompatActivity.getResources().getIdentifier(image, "mipmap", GlobalVariables.appCompatActivity.getPackageName()));
-            }
-
-            int[] ret = new int[imagesList.size()];
-            for(int i = 0;i < ret.length;i++)
-                ret[i] = imagesList.get(i);
-
-            images = ret;
 
             animal_card_image = (ImageView) itemView.findViewById(R.id.animal_card_image);
             animalName = (TextView) itemView.findViewById(R.id.animal_card_text);
-
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     int pos = getAdapterPosition();
 
-                    Intent intent = new Intent(GlobalVariables.appCompatActivity, AnimalActivity.class);
+                    Intent intent = new Intent(GlobalVariables.appCompatActivity, PersonalPopUp.class);
                     Bundle clickedAnimal = new Bundle();
-                    clickedAnimal.putInt("image", images[pos]); //Clicked image
-                    clickedAnimal.putString("name", animals[pos].getName());
-                    clickedAnimal.putSerializable("animal", animals[pos]);
-
+                    clickedAnimal.putSerializable("animal", personalStories[pos]);
+                    intent.putExtra("image", images[pos]);
                     intent.putExtras(clickedAnimal);
                     GlobalVariables.appCompatActivity.startActivity(intent);
                 }
@@ -82,13 +72,33 @@ public class PersonalStoriesRecyclerAdapter extends RecyclerView.Adapter<Persona
 
     @Override
     public void onBindViewHolder(PersonalStoriesRecyclerAdapter.ViewHolder viewHolder, int i) {
-        viewHolder.animalName.setText(animals[i].getName());
-        viewHolder.animal_card_image.setImageResource(images[i]);
+        final int width = 200;
+        final int height = 200;
+
+        viewHolder.animalName.setText(personalStories[i].getName());
+
+        GlobalVariables.bl.getImage(personalStories[i].getPictureUrl(), width, height, new GetObjectInterface() {
+            @Override
+            public void onSuccess(Object response) {
+                viewHolder.animal_card_image.setImageBitmap((Bitmap) response);
+                images[i] = (Bitmap) response;
+            }
+
+            @Override
+            public void onFailure(Object response) {
+                LinearLayout.LayoutParams layoutParams = new  LinearLayout.LayoutParams(width, height);
+                viewHolder.animal_card_image.setLayoutParams(layoutParams);
+                viewHolder.animal_card_image.setImageResource(R.mipmap.no_image_available);
+                images[i] = null;
+            }
+        });
+
+
     }
 
     @Override
     public int getItemCount() {
-        return animals.length;
+        return personalStories.length;
     }
 
 }
