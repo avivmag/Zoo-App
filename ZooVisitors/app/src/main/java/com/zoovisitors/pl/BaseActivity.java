1758dd33547ee9e68d5aee01f547979330004c99
gilorisr <gilorisr@post.bgu.ac.il>
2018-06-02 20:17:24 +0300

    lock screen rotation

--- ZooVisitors/app/src/main/java/com/zoovisitors/pl/BaseActivity.java
+++ ZooVisitors/app/src/main/java/com/zoovisitors/pl/BaseActivity.java
@@ -2,11 +2,14 @@ package com.zoovisitors.pl;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -48,13 +51,9 @@ public class BaseActivity extends AppCompatActivity {
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
     }
 
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-//                                           @NonNull int[] grantResults) {
-//        switch (requestCode) {
-//            case PERMISSION_REQUEST_GPS:
-//                GlobalVariables.gpsService.beginFlow();
-//                break;
-//        }
-//    }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
 }
