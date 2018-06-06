38f27b70c3fae4b5caa5811c1c9561a88e89ef85
orp <oripapo@gmail.com>
2018-06-06 23:45:56 +0300

    Started work on site refactor and confirm messages.

--- NegevZoo/WorkersSite/mainMenu/animal/animal.component.js
+++ NegevZoo/WorkersSite/mainMenu/animal/animal.component.js
@@ -1,32 +1,44 @@
 ﻿app.controller('zooAnimalCtrl', ['$q', '$state', '$scope', '$stateParams', 'utilitiesService', 'animalService', 'fileUpload',
-
     function animalController($q, $state, $scope, $stateParams, utilitiesService, animalService, fileUpload) {
+        // If no enclosure was given in the state params, return to the enclosure's list.
         if (!angular.isDefined($stateParams.enclosure)) {
             $state.go('mainMenu.enclosures.list');
         }
 
+        // Get all languages.
         app.getLanguages().then(
             (data) => {
                 $scope.languages    = data.data;
                 $scope.language     = $scope.languages[0];
                 
+                // Initialize the animal's component.
                 initializeComponent();
+
+                // Run animal's update.
                 $scope.updateAnimals();
             });
 
+   
+        // Initializes the animal component.
         function initializeComponent() {
             $scope.isLoading        = true;
 
+            // If an animal was given, set 'edit' mode to true.
             if (angular.isDefined($stateParams.animal)) {
                 $scope.isEdit       = true;
             }
 
+            // Get all state parameters to the scope.
             $scope.enclosure        = $stateParams.enclosure;
             $scope.baseURL          = app.baseURL;
             $scope.animal           = $stateParams.animal || { enclosureId: $scope.enclosure.id };
+
+            // Get the preservation types.
             $scope.preservation     = animalService.getPreservation();
 
+            // Initialize update animals function.
             $scope.updateAnimals    = function () {
+                // If this is edit mode, get the animal's details from the server.
                 if ($scope.isEdit) {
                     $scope.isLoading                = true;
 
@@ -34,12 +46,14 @@
                         function (data) {
                             $scope.animalDetails    = data.data;
     
-                            for (let i = 1; i <= 4; i++) {
+                            // Fill details from all languages.
+                            for (let i = 1; i <= $scope.languages.length; i++) {
                                 if (!$scope.animalDetails.some(ed => ed.language === i)) {
                                     $scope.animalDetails.push({ animalId: $scope.animal.id, language: i });
                                 }
                             }
     
+                            // Sort the data by languages.
                             $scope.animalDetails.sort(function(a, b) { return a.language-b.language; });
     
                             $scope.isLoading    = false;
@@ -55,97 +69,108 @@
                 }
             };
 
+            // Initialize add animal function.
             $scope.addAnimal        = function(animal) {
+                // If the animal check failed, return.
                 if (!checkAnimal(animal)) {
                     return;
                 }
 
-                $scope.isLoading            = true;
-                    var successContent      = !$scope.isEdit ? 'החיה נוספה בהצלחה!' : 'החיה עודכנה בהצלחה!';
-                    var failContent         = !$scope.isEdit ? 'התרחשה שגיאה בעת שמירת החיה' : 'התרחשה שגיאה בעת עדכון החיה';
+                $scope.isLoading        = true;
 
-                    var pictureUploadQuery  = uploadProfilePicture($scope.profilePic, animal);
+                // Initialize return statements.
+                var successContent      = !$scope.isEdit ? 'החיה נוספה בהצלחה!' : 'החיה עודכנה בהצלחה!';
+                var failContent         = !$scope.isEdit ? 'התרחשה שגיאה בעת שמירת החיה' : 'התרחשה שגיאה בעת עדכון החיה';
 
-                    var uploadPromises      = [pictureUploadQuery];
+                // Update the animal's profile picture (if needed).
+                var pictureUploadQuery  = uploadProfilePicture($scope.profilePic, animal);
 
-                    $q.all(uploadPromises).then(
-                        () => {
-                            animalService.updateAnimal(animal).then(
-                                function (response) {
-                                    utilitiesService.utilities.alert(successContent);
-                                    
-                                    $scope.isLoading        = false;
+                var uploadPromises      = [pictureUploadQuery];
 
-                                    $stateParams.animal     = response.data;
-                                    $stateParams.enclosure  = $scope.enclosure;
+                $q.all(uploadPromises).then(
+                    () => {
+                        // Update the animal.
+                        animalService.updateAnimal(animal).then(
+                            function (response) {
+                                utilitiesService.utilities.alert(successContent);
+                                
+                                $scope.isLoading        = false;
 
-                                    initializeComponent();
-                                    $scope.updateAnimals();
-                                },
-                                function () {
-                                    utilitiesService.utilities.alert(failContent);
-        
-                                    $scope.isLoading = false;
-                                });
-                        },
-                        () => {
-                            utilitiesService.utilities.alert(failContent);
+                                // Refresh the data.
+                                $stateParams.animal     = response.data;
+                                $stateParams.enclosure  = $scope.enclosure;
 
-                            $isLoading = false;
-                        });
+                                // Refresh the component.
+                                initializeComponent();
+                                $scope.updateAnimals();
+                            },
+                            function () {
+                                utilitiesService.utilities.alert(failContent);
+    
+                                $scope.isLoading = false;
+                            });
+                    },
+                    () => {
+                        utilitiesService.utilities.alert(failContent);
+
+                        $isLoading = false;
+                    });
             };
 
-            $scope.deleteAnimal     = function(animalId) {
-                $scope.isLoading                    = true;
-
-                animalService.deleteAnimal(animalId).then(
-                    function() {
-                        utilitiesService.utilities.alert("החיה נמחקה בהצלחה!");
-
-                        $state.go('mainMenu.enclosures.enclosure', { enclosure: $scope.enclosure });
-                    },
-
+            // Confirm's a deletion of an animal.
+            $scope.confirmDeleteAnimal = function(animalId) {
+                utilitiesService.utilities.confirm({ title: 'מחיקת חיה', text: 'האם אתה בטוח שברצונך למחוק חיה זו?' }).then(
                     function () {
-                        utilitiesService.utilities.alert("חלה שגיאה במחיקת החיה.");
-
-                        $scope.isLoading            = false;
+                        deleteAnimal(animalId);
                     });
+
+                return;
             }
 
+            // Initialize add animal detail function.
             $scope.addAnimalDetail       = function(animalDetail) {
                 $scope.isLoading            = true;
-                    var successContent      = $scope.page === 'create' ? 'החיה נוספה בהצלחה!' : 'החיה עודכנה בהצלחה!';
-                    var failContent         = $scope.page === 'create' ? 'התרחשה שגיאה בעת שמירת החיה' : 'התרחשה שגיאה בעת עדכון החיה';
 
-                    var audioUploadQuery    = uploadAudioFile(animalDetail);
+                // Initialize the return statements.
+                var successContent      = $scope.page === 'create' ? 'החיה נוספה בהצלחה!' : 'החיה עודכנה בהצלחה!';
+                var failContent         = $scope.page === 'create' ? 'התרחשה שגיאה בעת שמירת החיה' : 'התרחשה שגיאה בעת עדכון החיה';
 
-                    $q.all([audioUploadQuery]).then(
-                        () => {
-                            animalService.updateAnimalDetail(animalDetail).then(
-                                function () {
-                                    utilitiesService.utilities.alert(successContent);
+                // Upload audio file (if needed).
+                var audioUploadQuery    = uploadAudioFile(animalDetail);
 
-                                    delete $scope.audio;
+                $q.all([audioUploadQuery]).then(
+                    () => {
+                        // Update the animal detail.
+                        animalService.updateAnimalDetail(animalDetail).then(
+                            function () {
+                                utilitiesService.utilities.alert(successContent);
 
-                                    $scope.isLoading = false;
-                                },
-                                function () {
-                                    utilitiesService.utilities.alert(failContent);
+                                // Delete the audio object that the controller holds.
+                                delete $scope.audio;
 
-                                    $scope.isLoading = false;
-                                });
-                        },
-                        () => utilitiesService.utilities.alert(failContent));
+                                $scope.isLoading = false;
+                            },
+                            function () {
+                                utilitiesService.utilities.alert(failContent);
+
+                                $scope.isLoading = false;
+                            });
+                    },
+                    () => utilitiesService.utilities.alert(failContent));
             }
 
+            // Plays or stops the audio file.
             $scope.playSound                = function(audioFile) {
+                // If the controller does not hold an audio instance, initialize one.
                 if (!$scope.audio) {
                     $scope.audio = new Audio($scope.baseURL + audioFile);
                 }
                 
+                // If the audio instance is paused, play it.
                 if ($scope.audio.paused) {
                     $scope.audio.play();
                 }
+                // Otherwise, pause the audio instance and set it's time to 0.
                 else {
                     $scope.audio.pause();
                     $scope.audio.currentTime    = 0;
@@ -153,17 +178,42 @@
             };
         };
 
+        // Deletes an animal.
+        function deleteAnimal(animalId) {
+            $scope.isLoading                    = true;
+
+            // Delete the animal.
+            animalService.deleteAnimal(animalId).then(
+                function() {
+                    utilitiesService.utilities.alert("החיה נמחקה בהצלחה!");
+
+                    // Go back the the enclosure page.
+                    $state.go('mainMenu.enclosures.enclosure', { enclosure: $scope.enclosure });
+                },
+
+                function () {
+                    utilitiesService.utilities.alert("חלה שגיאה במחיקת החיה.");
+
+                    $scope.isLoading            = false;
+                });
+        }
+
+        
+        // Checks if the animal input is valid.
         function checkAnimal(animal) {
+            // If no animal was given, return.
             if (!animal) {
                 return false;
             }
 
+            // If the animal does not have a name, or it is whitespace, return.
             if (!angular.isDefined(animal.name) || animal.name == '') {
                 utilitiesService.utilities.alert('אנא בחר שם לחיה');
 
                 return false;
             }
 
+            // If the animal does not have preservation, return.
             if (!angular.isDefined(animal.preservation)) {
                 utilitiesService.utilities.alert('אנא בחר רמת שימור לחיה');
 
@@ -173,17 +223,22 @@
             return true;
         }
 
+        // Uploads an audio file.
         function uploadAudioFile(animalDetail) {
+            // If no audio file was given, return.
             if (!angular.isDefined(animalDetail.animalAudioFile) || animalDetail.animalAudioFile === null) {
                 return;
             }
 
             $scope.isLoading        = true;
 
+            // Set the upload URL.
             var uploadUrl           = 'animals/upload/audio/false';
 
+            // Upload the file.
             var fileUploadQuery     = fileUpload.uploadFileToUrl(animalDetail.animalAudioFile, uploadUrl).then(
                 (success)   => {
+                    // Retrieve the audio file's URL.
                     animalDetail.audioUrl    = success.data[0];
                 },
                 ()          => {
@@ -193,19 +248,25 @@
             return fileUploadQuery;
         }
 
+        // Uploads a profile picture.
         function uploadProfilePicture (picture, animal) {
+            // If no profile picture was given, return.
             if (!angular.isDefined(picture)) {
                 return;
             }
 
             $scope.isLoading        = true;
 
+            // Set the upload URL.
             var uploadUrl           = 'animals/upload/pictures';
 
+            // Upload the file.
             var fileUploadQuery     = fileUpload.uploadFileToUrl(picture, uploadUrl).then(
                 (success)   => {
+                    // Retrieve the picture's URL.
                     animal.pictureUrl    = success.data[0];
 
+                    // Set the file object to null.
                     $scope.profilePic       = null;
                 },
                 ()          => {
--- NegevZoo/WorkersSite/mainMenu/animalStory/animalStory.component.js
+++ NegevZoo/WorkersSite/mainMenu/animalStory/animalStory.component.js
@@ -1,44 +1,57 @@
 ﻿app.controller('zooAnimalStoryCtrl', ['$q', '$state', '$scope', '$stateParams', 'utilitiesService', 'animalService', 'fileUpload',
 
     function animalStoryController($q, $state, $scope, $stateParams, utilitiesService, animalService, fileUpload) {
+        // If no enclosure was given in the state params, return to the enclosure's list.
         if (!angular.isDefined($stateParams.enclosure)) {
             $state.go('mainMenu.enclosures.list');
         }
 
+        // Get all languages.
         app.getLanguages().then(
             (data) => {
                 $scope.languages    = data.data;
                 $scope.language     = $scope.languages[0];
                 
+                // Initialize the animal story component.
                 initializeComponent();
+
+                // Run animal stories update.
                 $scope.updateAnimalStories();
             });
 
+        // Initializes the animal story component.
         function initializeComponent() {
             $scope.isLoading        = true;
 
+            // If an animal story was given, set 'edit' mode as true.
             if (angular.isDefined($stateParams.animalStory)) {
                 $scope.isEdit       = true;
             }
 
+            // Get all state parameters to the scope.
             $scope.enclosure        = $stateParams.enclosure;
             $scope.baseURL          = app.baseURL;
             $scope.animalStory      = $stateParams.animalStory || { enclosureId: $scope.enclosure.id };
 
+            // Initializes update animal stories function.
             $scope.updateAnimalStories    = function () {
+                // If this is edit mode, get the story details from the server.
                 if ($scope.isEdit) {
                     $scope.isLoading                = true;
 
+                    // Get the story details.
                     animalService.getAnimalStoryDetailsById($scope.animalStory.id).then(
                         function (data) {
                             $scope.animalStoryDetails    = data.data;
     
-                            for (let i = 1; i <= 4; i++) {
+                            // Fill details from all languages.
+                            for (let i = 1; i <= app.languages.length; i++) {
                                 if (!$scope.animalStoryDetails.some(ed => ed.language === i)) {
                                     $scope.animalStoryDetails.push({ animalStoryId: $scope.animalStory.id, language: i });
                                 }
                             }
     
+                            // Sort the data by languages.
                             $scope.animalStoryDetails.sort(function(a, b) { return a.language-b.language; });
     
                             $scope.isLoading    = false;
@@ -54,87 +67,111 @@
                 }
             };
 
+            // Initializes the add animal story function.
             $scope.addAnimalStory   = function(animalStory) {
+                // If the animal check failed, return.
                 if (!checkAnimalStory(animalStory)) {
                     return;
                 }
 
                 $scope.isLoading            = true;
-                    var successContent      = !$scope.isEdit ? 'הסיפור האישי נוסף בהצלחה!' : 'הסיפור האישי עודכן בהצלחה!';
-                    var failContent         = !$scope.isEdit ? 'התרחשה שגיאה בעת שמירת הסיפור האישי' : 'התרחשה שגיאה בעת עדכון הסיפור האישי';
 
-                    var pictureUploadQuery  = uploadProfilePicture($scope.profilePic, animalStory);
+                // Initialize return statements.
+                var successContent      = !$scope.isEdit ? 'הסיפור האישי נוסף בהצלחה!' : 'הסיפור האישי עודכן בהצלחה!';
+                var failContent         = !$scope.isEdit ? 'התרחשה שגיאה בעת שמירת הסיפור האישי' : 'התרחשה שגיאה בעת עדכון הסיפור האישי';
 
-                    var uploadPromises      = [pictureUploadQuery];
+                // Upload a profile picture (If needed).
+                var pictureUploadQuery  = uploadProfilePicture($scope.profilePic, animalStory);
 
-                    $q.all(uploadPromises).then(
-                        () => {
-                            animalService.updateAnimalStory(animalStory).then(
-                                function (response) {
-                                    utilitiesService.utilities.alert(successContent);
-                                    
-                                    $scope.isLoading            = false;
+                var uploadPromises      = [pictureUploadQuery];
 
-                                    $stateParams.animalStory    = response.data;
-                                    $stateParams.enclosure      = $scope.enclosure;
+                $q.all(uploadPromises).then(
+                    () => {
+                        // Update the animal story.
+                        animalService.updateAnimalStory(animalStory).then(
+                            function (response) {
+                                utilitiesService.utilities.alert(successContent);
+                                
+                                $scope.isLoading            = false;
 
-                                    initializeComponent();
-                                    $scope.updateAnimalStories();
-                                },
-                                function () {
-                                    utilitiesService.utilities.alert(failContent);
-        
-                                    $scope.isLoading = false;
-                                });
-                        },
-                        () => {
-                            utilitiesService.utilities.alert(failContent);
+                                // Refresh the data.
+                                $stateParams.animalStory    = response.data;
+                                $stateParams.enclosure      = $scope.enclosure;
 
-                            $isLoading = false;
-                        });
+                                // Refresh the component.
+                                initializeComponent();
+                                $scope.updateAnimalStories();
+                            },
+                            function () {
+                                utilitiesService.utilities.alert(failContent);
+    
+                                $scope.isLoading = false;
+                            });
+                    },
+                    () => {
+                        utilitiesService.utilities.alert(failContent);
+
+                        $isLoading = false;
+                    });
             };
 
-            $scope.deleteAnimalStory = function(animalStoryId) {
-                $scope.isLoading                    = true;
-
-                animalService.deleteAnimalStory(animalStoryId).then(
-                    function() {
-                        utilitiesService.utilities.alert("הסיפור האישי נמחק בהצלחה!");
-
-                        $state.go('mainMenu.enclosures.enclosure', { enclosure: $scope.enclosure });
-                    },
-
+            $scope.confirmDeleteAnimalStory = function(animalStoryId) {
+                utilitiesService.utilities.confirm({ title: 'מחיקת סיפור אישי', text: 'האם אתה בטוח שברצונך למחוק את הסיפור האישי?'}).then(
                     function () {
-                        utilitiesService.utilities.alert("חלה שגיאה במחיקת הסיפור האישי.");
-
-                        $scope.isLoading            = false;
+                        deleteAnimalStory(animalStoryId);
                     });
             }
 
+            // Initialize the add animal story detail function.
             $scope.addAnimalStoryDetail       = function(animalStoryDetail) {
                 $scope.isLoading            = true;
-                    var successContent      = !$scope.isEdit ? 'החיה נוספה בהצלחה!' : 'החיה עודכנה בהצלחה!';
-                    var failContent         = !$scope.isEdit ? 'התרחשה שגיאה בעת שמירת החיה' : 'התרחשה שגיאה בעת עדכון החיה';
+                
+                // Initialize the return statements.
+                var successContent      = !$scope.isEdit ? 'החיה נוספה בהצלחה!' : 'החיה עודכנה בהצלחה!';
+                var failContent         = !$scope.isEdit ? 'התרחשה שגיאה בעת שמירת החיה' : 'התרחשה שגיאה בעת עדכון החיה';
 
-                    animalService.updateAnimalStoryDetail(animalStoryDetail).then(
-                        function () {
-                            utilitiesService.utilities.alert(successContent);
+                // Update the story detail.
+                animalService.updateAnimalStoryDetail(animalStoryDetail).then(
+                    function () {
+                        utilitiesService.utilities.alert(successContent);
 
-                            $scope.isLoading = false;
-                        },
-                        function () {
-                            utilitiesService.utilities.alert(failContent);
+                        $scope.isLoading = false;
+                    },
+                    function () {
+                        utilitiesService.utilities.alert(failContent);
 
-                            $scope.isLoading = false;
-                        });
+                        $scope.isLoading = false;
+                    });
             }
         };
 
+        // Deletes an animal story.
+        function deleteAnimalStory(animalStoryId) {
+            $scope.isLoading                    = true;
+
+            // Delete the animal.
+            animalService.deleteAnimalStory(animalStoryId).then(
+                function() {
+                    utilitiesService.utilities.alert("הסיפור האישי נמחק בהצלחה!");
+
+                    $state.go('mainMenu.enclosures.enclosure', { enclosure: $scope.enclosure });
+                },
+
+                function () {
+                    utilitiesService.utilities.alert("חלה שגיאה במחיקת הסיפור האישי.");
+
+                    $scope.isLoading            = false;
+                });
+        }
+
+        // Checks the animal story validity.
         function checkAnimalStory(checkAnimalStory) {
+            // If no animal story was given, return.
             if (!checkAnimalStory) {
                 return false;
             }
 
+            // If the animal story did not have a name, or whitespace, return.
             if (!angular.isDefined(checkAnimalStory.name) || checkAnimalStory.name == '') {
                 utilitiesService.utilities.alert('אנא בחר שם לסיפור החיה');
 
@@ -144,19 +181,25 @@
             return true;
         }
 
+        // Uploads a profile picture.
         function uploadProfilePicture (picture, animalStory) {
+            // If no picture was given, return.
             if (!angular.isDefined(picture)) {
                 return;
             }
 
             $scope.isLoading        = true;
 
+            // Initialize the upload URL.
             var uploadUrl           = 'animals/upload/story/';
 
+            // Upload the profile picture.
             var fileUploadQuery     = fileUpload.uploadFileToUrl(picture, uploadUrl).then(
                 (success)   => {
+                    // Retrieve the picture's URL.
                     animalStory.pictureUrl  = success.data[0];
 
+                    // Set the profile picture that the component holds to null.
                     $scope.profilePic       = null;
                 },
                 ()          => {
