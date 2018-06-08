8fe78e8c73f7f3d41c6b424f29d60812c3c1ee99
orp <oripapo@gmail.com>
2018-06-08 18:10:50 +0300

    Finished refactoring site's code.

--- NegevZoo/WorkersSite/mainMenu/enclosuresList/enclosuresList.component.js
+++ NegevZoo/WorkersSite/mainMenu/enclosuresList/enclosuresList.component.js
@@ -1,14 +1,18 @@
-﻿app.controller('zooEnclosuresListCtrl', ['$q', '$scope', '$mdDialog', 'utilitiesService', 'enclosureService', 'animalService', 'mapViewService', 'fileUpload',
-    function enclosureListController($q, $scope, $mdDialog, utilitiesService, enclosureService, animalService, mapViewService, fileUpload) {
+﻿app.controller('zooEnclosuresListCtrl', ['$scope', 'utilitiesService', 'enclosureService',
+    function enclosureListController($scope, utilitiesService, enclosureService) {
+        // Initialize the enclosure list component.
         initializeComponent();
 
+        // Update the enclosures.
         $scope.updateEnclosures();
         
         function initializeComponent() {
             $scope.isLoading                = true;
             $scope.baseURL                  = app.baseURL;
 
+            // Initialize the update enclosure function.
             $scope.updateEnclosures         = function () {
+                // Get the enclosures.
                 enclosureService.enclosures.getAllEnclosures().then(
                     function (data) {
                         $scope.enclosures   = data.data;
--- NegevZoo/WorkersSite/mainMenu/mapView/mapView.service.js
+++ NegevZoo/WorkersSite/mainMenu/mapView/mapView.service.js
@@ -1,18 +1,22 @@
 app.factory('mapViewService', ['$mdDialog', function ($mdDialog) {
     MapDialogController.$Inject = ['mapService', '$rootScope'];
 
-    function MapDialogController($scope, $mdDialog, selectedObject, selectedUrlProp, selectedUrlX, selectedUrlY, mapService, $rootScope) {
+    function MapDialogController($scope, $mdDialog, selectedObject, selectedUrlProp, selectedUrlX, selectedUrlY, markerToRemove, mapService, $rootScope) {
         $scope.isLoading    = true;
         
+        // Initialize the map.
         initializeMap();
 
+        // Initializes the map.
         function initializeMap() {
+            // Set all the scope's variables.
             $scope.img              = new Image();
             $scope.selectedObject   = selectedObject;
             $scope.selectedUrlProp  = selectedUrlProp;
             $scope.baseURL          = app.baseURL;
             $scope.mapStyle         = { };
 
+            // Whenever the map image finishes loading, get the map's resize ratio, and set its style.
             $scope.img.onload       = function () {
                 $scope.ratio                = (Math.max($scope.img.width, $scope.img.height) / 480.0);
 
@@ -22,6 +26,7 @@ app.factory('mapViewService', ['$mdDialog', function ($mdDialog) {
 
                 $scope.isLoading            = false;
                 
+                // Get the map's icons.
                 getIcons();
             };
 
@@ -42,6 +47,7 @@ app.factory('mapViewService', ['$mdDialog', function ($mdDialog) {
                     ratio:  $scope.ratio
                 };
 
+                // Set the marker's style.
                 selectedObject.markerStyle  = {
                     position:       'fixed',
                     top:            Math.floor($scope.clickPosition.height - ($scope.selectedObjectHeight / (2 * $scope.ratio))) + 'px',
@@ -52,25 +58,30 @@ app.factory('mapViewService', ['$mdDialog', function ($mdDialog) {
                     borderColor:    'yellow'
                 }
 
+                // Set the selected object load to be done.
                 $scope.selectedObjectDone = true;
             }
             
+            // Initializes the close function.
             $scope.close    = function(clickPosition) {
                 // Return the offset when the dialog closes.
                 $mdDialog.hide(clickPosition);
             }
         }
 
+        // Get the map's icons.
         function getIcons() {
             var iconsQuery = mapService.getAllMarkers().then(
                 function (data) {
-                    
-                    let markers = data.data.filter(marker => marker.iconUrl != selectedObject[selectedUrlProp]);
+                    // Remove the selected marker from the map markers. (So it will not show on the map)
+                    let markers = data.data.filter(marker => marker.iconUrl != (markerToRemove ? markerToRemove : ''));
                     
                     var selectedObjectImage = new Image();
 
+                    // Get the image of the selected object.
                     selectedObjectImage.src = app.baseURL + selectedObject[selectedUrlProp];
 
+                    // When the image finishes loading, set it's style on the map.
                     selectedObjectImage.onload = function () {
                         $scope.selectedObjectHeight    = selectedObjectImage.height;
                         $scope.selectedObjectWidth     = selectedObjectImage.width;
@@ -83,25 +94,31 @@ app.factory('mapViewService', ['$mdDialog', function ($mdDialog) {
                                 position:   'fixed',
                                 left:       leftOffset + 'px',
                                 top:        topOffset + 'px',
-                                width:      Math.floor($scope.selectedObjectHeight / $scope.ratio) + 'px',
-                                height:     Math.floor($scope.selectedObjectWidth / $scope.ratio) + 'px',
+                                width:      Math.floor($scope.selectedObjectWidth / $scope.ratio) + 'px',
+                                height:     Math.floor($scope.selectedObjectHeight / $scope.ratio) + 'px',
                                 borderStyle: 'ridge',
                                 borderColor: 'yellow'
                             };
 
+                            // Set the object to done.
                             $scope.selectedObjectDone = true;
+                            
+                            // Apply the changes.
                             $rootScope.$apply();
                         }
                     }
 
                     if (angular.isArray(markers)) {
+                        // Initialize the scope's markers array.
                         $scope.markers  = [];
                         
                         markers.map(marker => {
                             var markerImage = new Image();
 
+                            // Get the image of the marker.
                             markerImage.src = app.baseURL + marker.iconUrl;
 
+                            // When the marker finishes loading, set it's style on the map.
                             markerImage.onload = function () {
                                 var markerHeight    = markerImage.height;
                                 var markerWidth     = markerImage.width;
@@ -112,11 +129,12 @@ app.factory('mapViewService', ['$mdDialog', function ($mdDialog) {
                                         position:   'fixed',
                                         left:       Math.floor(((marker.longitude - (markerImage.width / 2)) / $scope.ratio)) + 'px',
                                         top:        Math.floor(((marker.latitude - (markerImage.height / 2)) / $scope.ratio)) + 'px',
-                                        width:      Math.floor(markerHeight / $scope.ratio) + 'px',
-                                        height:     Math.floor(markerWidth / $scope.ratio) + 'px'
+                                        width:      Math.floor(markerWidth / $scope.ratio) + 'px',
+                                        height:     Math.floor(markerHeight / $scope.ratio) + 'px'
                                     }
                                 });
 
+                                // Apply the changes.
                                 $rootScope.$apply();
                             }
                         });
@@ -126,8 +144,9 @@ app.factory('mapViewService', ['$mdDialog', function ($mdDialog) {
             return iconsQuery;
         }
     }
-       
-    function showMap(ev, selectedObject, selectedUrlProp, selectedUrlX, selectedUrlY) {
+
+    // Shows the map.
+    function showMap(ev, selectedObject, selectedUrlProp, selectedUrlX, selectedUrlY, markerToRemove) {
         var mapDialog = $mdDialog.show({
             controller:             MapDialogController,
             templateUrl:            'mainMenu/mapView/map.dialog.html',
@@ -138,13 +157,15 @@ app.factory('mapViewService', ['$mdDialog', function ($mdDialog) {
                 selectedObject,
                 selectedUrlProp,
                 selectedUrlX,
-                selectedUrlY
+                selectedUrlY,
+                markerToRemove
             }
         });
 
         return mapDialog;
     }
     
+    // Initialize the service's exported methods.
     var mapViewService      = {
         showMap: showMap
     };
